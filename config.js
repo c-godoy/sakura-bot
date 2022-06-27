b539c5aafb472f3bb6c1755d832881c514b89818
Cande <63575619+c-godoy@users.noreply.github.com>
2022-06-27 17:18:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-    consumer_key:         'Ut6XdTPku0PX7M7ZjABXaGRU3',
-    consumer_secret:      'YuJGqfvgcxcBkmAM0rHA9NlVg30JwpV2tJ4v6vNgY5mGJwbL9R',
-    access_token:         '1529501745860300803-lFPLl5wEfMOTpwr27OhdWw7y43ZbJj',
-    access_token_secret:  'uhE9TWP2S6rOLF5QPPOTH0rhYry5ZiIxymUKJ8glwvuA9'
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  ''
   }
   
   module.exports = config;
